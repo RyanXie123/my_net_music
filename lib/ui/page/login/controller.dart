25823d5cf7dcad308aa9d1e4806ca5395d69bd0f
谢汝 <raxieru@outlook.com>
2022-11-21 09:37:08 +0800

    update

--- lib/ui/page/login/controller.dart
+++ lib/ui/page/login/controller.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
+import 'package:my_net_music/res/r.dart';
+import 'package:video_player/video_player.dart';
 
-class LoginController extends GetxController {}
+class LoginController extends GetxController {
+  late VideoPlayerController videoController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    videoController = VideoPlayerController.asset(R.videos.loginVideo)
+      ..initialize().then((value) {
+        videoController.setLooping(true);
+        videoController.play();
+        update();
+      });
+  }
+
+  login() {}
+}
--- lib/ui/page/login/page.dart
+++ lib/ui/page/login/page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 import 'controller.dart';
 
 class LoginPage extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
+    return LoginBg(
+        child: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 120, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    ));
+  }
+
+  Scaffold LoginBg({required Widget child}) {
     return Scaffold(
-      appBar: AppBar(title: Text('LoginPage')),
-      body: SafeArea(child: Text('LoginController')),
+      body: Stack(children: [
+        GetBuilder<LoginController>(
+          builder: (controller) => VideoPlayer(controller.videoController),
+        ),
+        child
+      ]),
     );
   }
 }
